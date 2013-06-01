32c529e80129e491aaf04a269ed217497216addd
Kimmo Huoman <kipenroskaposti@gmail.com>
2013-06-01 04:18:24 +0300

    working setup script with minimum requirements, requirements for rt_sim in rt_sim/requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='rt_client',
     version='0.1',
     packages=['rt_client'],
+    install_requires=[
+        'requests>=1.2.3',
+    ]
 )
